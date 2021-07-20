bbea7a490492a46586a179404f36bbc59bbbbe11
Craig Neff <ifly12610@gmail.com>
2021-07-20 01:30:31 -0600

    fixed a tiny error in the array shallow copy

--- script.js
+++ script.js
@@ -113,15 +113,15 @@ const jDataOne = [3, 5, 2, 12, 7];
 const kDataOne = [4, 1, 15, 8, 3];
 const jDataTwo = [9, 16, 6, 8, 3];
 const kDataTwo = [10, 5, 6, 1, 4];
-
-let jDataNewOne = jDataOne.slice(1, -1);
+/*
+let jDataNewOne = jDataOne.slice(1, -2);
 
 console.log(jDataNewOne);
 
-/* console.log(jDataOne.slice(1, -1)) */
+ console.log(jDataOne.slice(1, -1)) */
 
 function checkDogs(dogsJulia, dogsKate) {
-  let noCats = dogsJulia.slice(1, -1);
+  let noCats = dogsJulia.slice(1, -2);
   let allData = noCats.concat(dogsKate);
   allData.forEach(function (age, i) {
     if (age >= 3) {
@@ -131,3 +131,5 @@ function checkDogs(dogsJulia, dogsKate) {
     }
   });
 }
+checkDogs(jDataOne, kDataOne);
+checkDogs(jDataTwo, kDataTwo);
