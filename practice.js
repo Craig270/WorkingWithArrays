d66b59d78a0e472b6d45f9880b8a69bd05467fff
Craig Neff <ifly12610@gmail.com>
2021-07-28 22:35:03 -0600

    worked on a more practice with map, filter, reduce methods

--- practice.js
+++ practice.js
@@ -40,3 +40,36 @@ function checkDogs(dogsJulia, dogsKate) {
 
 checkDogs(jDataOne, kDataOne);
 checkDogs(jDataTwo, kDataTwo);
+
+/* 
+Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
+
+Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
+
+1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4.
+2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
+3. Calculate the average human age of all adult dogs.
+4. Run the function for both test datasets.
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+*/
+console.log(`Code Challenge #2 below:`);
+
+dog1 = [5, 2, 4, 1, 15, 8, 3];
+dog2 = [16, 6, 10, 5, 6, 1, 4];
+
+function calcAverageHumanAge(arr) {
+  console.log(`Start of function`);
+  let humanDogAge = arr.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+  let filterDogs = humanDogAge.filter(age => (age >= 18 ? age : null));
+  let avgAge =
+    filterDogs.reduce((snowball, age) => snowball + age, 0) / filterDogs.length;
+  console.log(humanDogAge);
+  console.log(filterDogs);
+  console.log(avgAge);
+  console.log(`End of function`);
+}
+calcAverageHumanAge(dog1);
+calcAverageHumanAge(dog2);
